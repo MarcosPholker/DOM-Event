32f4b4faefeb78d27c245795389d3e98faf11eb0
Marcos Silva <antonioleitedasilvamarcos@gmail.com>
2023-03-07 16:34:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
-const ct1 = document.querySelector("#ct1")
-const curso = [...document.querySelectorAll(".curso")]
-const c1_2 = document.querySelector("#c1_2")
+const cursos = ["html", "css", "javaScript","python","react"]
+const caixaCurso = document.querySelector("#caixaCursos")
+
+const btnSelecionarCurso = document.getElementById("selecionarCurso")
+
+const btnRemoverCurso = document.getElementById("removerCurso")
+
+const btnAdcionarCursoAntes = document.getElementById("adcionarCurso")
+
+const inputNovoCurso = document.getElementById("textoNovoCurso")
+
+var indice = 0
+const criarNovoCurso=(curso)=>{
+    const novoElemento = document.createElement("div")
+    novoElemento.setAttribute("id", "novoE"+indice)
+    novoElemento.setAttribute("class", "curso c1")
+    novoElemento.innerHTML = curso;
+
+    const inputRadio = document.createElement("input")
+    inputRadio.setAttribute("type", "radio")
+    inputRadio.setAttribute("name", "radio")
+    inputRadio.setAttribute("id","radio"+indice)
 
 
-const cursos = ["HTML","CSS","JavaScript","PHP","MySQL","react Native"]
+    novoElemento.appendChild(inputRadio)
+    return novoElemento    
+}
 
-cursos.map((el, i)=>{
-    i++
-    const novosElemento = document.createElement("div")
-    novosElemento.setAttribute("id","c"+i)
-    novosElemento.setAttribute("class", "curso c1")
-    novosElemento.innerHTML = el
-
-    const btn_lixeira = document.createElement("img")
-    btn_lixeira.setAttribute("src","./pngwing.com.png")
-    btn_lixeira.setAttribute("class", "btn_lixeira")
-    btn_lixeira.addEventListener("click",(evt)=>{
-        ct1.removeChild(evt.target.parentNode)
-        console.log(evt.target.parentNode)
-    })
-
-    novosElemento.appendChild(btn_lixeira)
-    ct1.appendChild(novosElemento)
+cursos.map((el,chave)=>{
+    const novoElemento = criarNovoCurso(el)
+    caixaCurso.appendChild(novoElemento)
+    indice++
 })
 
 
+function radioSelecionado(){
+    const radioS = [...document.querySelectorAll("input[type=radio]")]
+    const radioschekados = radioS.filter((ck)=>{
+        return ck.checked
+    })
+    console.log(radioschekados[0])
+    return radioschekados[0]
+}
+
+btnSelecionarCurso.addEventListener("click",(evt)=>{
+    try{
+    const cs = radioSelecionado()
+    const cursoSelecionado = cs.parentNode.textContent
+    alert("curso "+ cursoSelecionado + " selecionado")
+    }catch(ex){
+        alert("selecione um curso")
+    }
+})
+
+
+btnRemoverCurso.addEventListener("click",(evt)=>{
+    try{
+    const rc = radioSelecionado();
+    const removerCurso = rc.parentNode
+    removerCurso.remove()
+    }catch(ex){
+        alert("selecione um curso")
+    }
+})
+
+btnAdcionarCursoAntes.addEventListener("click", (evt)=>{
+    const cs = radioSelecionado()
+    try{
+    const cursoSelecionado = cs.parentNode
+    const novoCurso = criarNovoCurso(inputNovoCurso.value)
+    caixaCurso.insertBefore(novoCurso,cursoSelecionado)
+    }catch(ex){
+        alert("selecione um curso")
+}
+})
